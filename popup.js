ffafa333e5bec97092faf7469abc28ad236cd37b
Medi Assumani <mediassumani49@gmail.com>
2019-01-10 17:44:02 -0800

    [Add] form for sign up

---
+++ popup.js
@@ -0,0 +1,6 @@
+// // Takes data from the DOM and sends it to the server
+
+const firstName = document.getElementById('first-name-input');
+const lastName = document.getElementById('last-name-input');
+const emailAddress = document.getElementById('email-address-input');
+const githubHandle = document.getElementById('github-handle-input');
