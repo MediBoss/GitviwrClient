3a17815ff6844e06a4059074469faf4b6ba06549
Medi Assumani <mediassumani49@gmail.com>
2019-01-13 16:30:00 -0800

    [Add] assets

--- popup.js
+++
@@ -1,6 +0,0 @@
-// // Takes data from the DOM and sends it to the server
-
-const firstName = document.getElementById('first-name-input');
-const lastName = document.getElementById('last-name-input');
-const emailAddress = document.getElementById('email-address-input');
-const githubHandle = document.getElementById('github-handle-input');
--- src/background/background.js
+++ src/background/background.js
@@ -13,3 +13,13 @@
 //     chrome.pageAction.show(tab.id);
 //     chrome.tabs.executeScript(null, {"file": "../../content.js"});
 // });
+
+
+chrome.extension.onConnect.addListener(function(port) {
+  //const socket = io('http://localhost:3000');
+  console.log("Connected .....");
+  port.onMessage.addListener(function(msg) {
+      console.log("message recieved" + msg);
+      // port.postMessage("Hi Popup.js");
+  });
+})
