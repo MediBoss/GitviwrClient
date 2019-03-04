c8fa4b1264bf32bcbe48c2e4eb02629fa8873f53
Medi Assumani <mediassumani49@gmail.com>
2019-03-03 16:49:05 -0800

    [Add] local storage to set the token

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
-//Returns the github handle of the visited gihub pages
 
+
+// Return the github handle of the visited gihub pages
 (function () {
   const socket = io('http://localhost:3000');
   var user_url = window.location.pathname.split('/');
@@ -8,3 +9,14 @@
     socket.emit('github event', user_github_handle)
   })
 }());
+
+// stores the encrypted token of a user from Github API through Gitviwr server
+(function() {
+
+  var socket = io("http://localhost:3000")
+  socket.on('user token', function(userToken){
+    chrome.storage.sync.set({"token": userToken}, function() {
+      console.log('Token has been succesfully stored');
+    })    
+  })
+})
